0b50ed8c2900c5531621bb8dbf100e8d9c51f8ce
Ally-R <ally.rogers.mail@gmail.com>
2020-02-22 12:18:04 -0600

    Static pages uniformly formatted, tests updated

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Render 'hello' by running the app
-  root 'application#hello'
+  root 'static_pages#about'
 end
